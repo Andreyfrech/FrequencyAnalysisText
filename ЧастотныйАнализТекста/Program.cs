ddb8150ee4072550f363ecfbdde173918ab15b27
frech <andreyka977@list.ru>
2021-08-17 23:57:47 +0300

    Revert "metod readText"
    This reverts commit 11218d214776d7aae80a41b67409c0ffb12cab52.

--- ЧастотныйАнализТекста/Program.cs
+++ ЧастотныйАнализТекста/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 
 namespace ЧастотныйАнализТекста
 {
@@ -7,40 +6,7 @@ namespace ЧастотныйАнализТекста
     {
         static void Main(string[] args)
         {
-            ReadText();
-        }
-
-        static void ReadText()
-        {
-            Console.WriteLine("Введите путь к файлу");
-            string path = Convert.ToString(Console.ReadLine());
-            try
-            {
-                using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
-                {
-                    string line;
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        Console.WriteLine(line);
-                        string[] s1 = line.Split();
-                        string ss;
-                        for (int j = 0; j < s1.Length; j++)
-                        {
-                            line = s1[j];
-                            for (int i = 0; i < line.Length - 2; i++)
-                            {
-                                ss = line.Substring(i, 3);
-                                Console.WriteLine(ss);
-                            }
-                        }
-                    }
-                }
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            
+            Console.WriteLine("Hello World!");
         }
     }
 }
