29627e739ddf9ca7f37e9dbe41777ad14ead8437
frech <andreyka977@list.ru>
2021-08-18 00:06:29 +0300

    metod ReadText

--- ЧастотныйАнализТекста/Program.cs
+++ ЧастотныйАнализТекста/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ЧастотныйАнализТекста
 {
@@ -6,7 +7,41 @@ namespace ЧастотныйАнализТекста
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ReadText();
+        }
+
+        static void ReadText()
+        {
+            Console.WriteLine("Введите путь к файлу");
+            string path = Convert.ToString(Console.ReadLine());
+            try
+            {
+                using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                        string[] s1 = line.Split();
+                        string ss;
+                        for (int j = 0; j < s1.Length; j++)
+                        {
+                            line = s1[j];
+                            for (int i = 0; i < line.Length - 2; i++)
+                            {
+                                ss = line.Substring(i, 3);
+                                Console.WriteLine(ss);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
+
