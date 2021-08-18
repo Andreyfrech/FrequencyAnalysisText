70c568fed129197f224f9804dc69c68eb70b61a1
frech <andreyka977@list.ru>
2021-08-18 23:34:37 +0300

    таймер

--- ЧастотныйАнализТекста/Program.cs
+++ ЧастотныйАнализТекста/Program.cs
@@ -1,29 +1,43 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace ЧастотныйАнализТекста
 {
     class Program
     {
+       static string path;
         static void Main(string[] args)
         {
-            ReadText();
+            new Program().ReadPath();
+           new Program().ReadText(path);
         }
 
-        static void ReadText()
+        public string ReadPath()
         {
             Console.WriteLine("Введите путь к файлу");
-            string path = Convert.ToString(Console.ReadLine());
+            path = Convert.ToString(Console.ReadLine());
+            return path;
+        }
+
+        // static void ReadText()
+        public void ReadText(string path)
+        {
+            
             try
             {
+              
                 using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
                 {
+                    var startTime = System.Diagnostics.Stopwatch.StartNew();
                     string line;
+                    string ss ="";
                     while ((line = sr.ReadLine()) != null)
                     {
+                       
                         Console.WriteLine(line);
                         string[] s1 = line.Split();
-                        string ss;
+                        
                         for (int j = 0; j < s1.Length; j++)
                         {
                             line = s1[j];
@@ -31,9 +45,13 @@ namespace ЧастотныйАнализТекста
                             {
                                 ss = line.Substring(i, 3);
                                 Console.WriteLine(ss);
+                                
                             }
                         }
                     }
+                   
+                    startTime.Stop();
+                    var resultTime = startTime.Elapsed;
                 }
             }
             catch (Exception e)
